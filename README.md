25582cf1b49d5a9a5a0e25477531a4370a9c6b3c
All Fiction <126734865+ziqkimi308@users.noreply.github.com>
2025-10-11 08:12:18 +0800

    Squashed 'pomodoro-timer/' content from commit c750e2f
    git-subtree-dir: pomodoro-timer
    git-subtree-split: c750e2fd2a312a8b3c476c751cd19985f35eb891

---
+++ README.md
@@ -0,0 +1,29 @@
+# Pomodoro Timer
+
+- The Pomodoro Timer is a productivity tool based on the Pomodoro Technique. It alternates between work sessions and breaks, helping you stay focused and organized. This timer app is built with Python and Tkinter and includes customizable work and break durations.
+
+---
+
+### Features:
+
+- Work and Break Sessions:
+  - Work Session: Default 2 minutes.
+  - Short Break: Default 1 minute.
+  - Long Break: Default 3 minutes. (Adjustable via constants WORK_MIN, SHORT_BREAK_MIN, and LONG_BREAK_MIN.)
+- Session Tracking:
+  - Displays checkmarks (✓) after each completed work session.
+- Skip Sessions:
+  - Manually skip the current session using the Skip button.
+- Reset Functionality:
+  - Reset the timer to its initial state with the Reset button.
+- Disabling Start Button:
+  - Prevents multiple simultaneous timers by disabling the Start button during a session.
+
+---
+
+### Screenshots:
+
+<img width="359" alt="image" src="https://github.com/user-attachments/assets/a942962d-f161-48b0-b764-1802aa959d04">
+<img width="355" alt="image" src="https://github.com/user-attachments/assets/a110041d-ce54-4636-9013-16731fbddd37">
+<img width="358" alt="image" src="https://github.com/user-attachments/assets/4b69d6e1-e752-4401-92e6-016fc6e5cf9d">
+<img width="359" alt="image" src="https://github.com/user-attachments/assets/271f4d25-b091-42cb-9fb1-d0a5847a589d">
---
+++ main.py
@@ -0,0 +1,106 @@
+# Import
+import math
+from tkinter import *
+
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 2
+SHORT_BREAK_MIN = 1
+LONG_BREAK_MIN = 3
+reps = 0
+timer = None
+
+# ---------------------------- TIMER RESET ------------------------------- # 
+def reset_timer():
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    label_timer.config(text="Timer")
+    label_check.config(text="")
+    global reps
+    reps = 0
+    
+	# Enabled the disabled start button
+    button_start.config(state=NORMAL)
+
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    global reps
+    reps += 1
+
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0:
+        count_down(long_break_sec)
+        label_timer.config(text="Break", fg=RED)
+    elif reps % 2 == 0:
+        count_down(short_break_sec)
+        label_timer.config(text="Break", fg=PINK)
+    else:
+        count_down(work_sec)
+        label_timer.config(text="Work", fg=GREEN)
+        
+	# Disable start button after clicked to avoid multiple clicks
+    button_start.config(state=DISABLED)
+        
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def count_down(count):
+    count_min = math.floor(count/60)
+    count_sec = count % 60
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        global timer
+        timer = window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+        marks = ""
+        work_sessions = math.floor(reps/2)
+        for _ in range(work_sessions):
+            marks += "✓"
+        label_check.config(text=marks)
+
+# ---------------------------- SKIP SESSION ------------------------------- #
+def skip_session():
+    global timer, reps
+    window.after_cancel(timer)
+    start_timer()
+
+# ---------------------------- UI SETUP ------------------------------- #
+# Tk class
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+# Canvas class
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file="./tomato.png")
+canvas.create_image(100, 112, image=tomato_img)
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(row=1, column=1)
+
+# Button class
+button_start = Button(text="Start", highlightthickness=0, command=start_timer)
+button_start.grid(row=2, column=0)
+
+button_reset = Button(text="Reset", highlightthickness=0, command=reset_timer)
+button_reset.grid(row=2, column=2)
+
+button_skip = Button(text="Skip", highlightthickness=0, command=skip_session)
+button_skip.grid(row=2, column=1)
+
+# Label class
+label_timer = Label(text="Timer", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 50, "bold"))
+label_timer.grid(row=0, column=1)
+
+label_check = Label(fg=GREEN, bg=YELLOW, font=(FONT_NAME, 20, "bold"))
+label_check.grid(row=3, column=1)
+
+# Last
+window.mainloop()
